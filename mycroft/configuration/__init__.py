3c5ee1e30b3e93a107a555e1e534d415b0b8c5e9
jarbasai <jarbasai@mailfence.com>
2017-12-14 23:51:24 +0000

    explicit path

--- mycroft/configuration/__init__.py
+++ mycroft/configuration/__init__.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from config import Configuration, LocalConf, RemoteConf, \
+from mycroft.configuration.config import Configuration, LocalConf, \
+    RemoteConf, \
                    SYSTEM_CONFIG, USER_CONFIG
 
 
