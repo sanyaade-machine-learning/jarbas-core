e37edd2f3aa10c86f941c4a81b4b9a12b4e77f77
jarbasai <jarbasai@mailfence.com>
2018-01-03 03:21:45 +0000

    websocket protocol

---
+++ mycroft/client/server/client.py
@@ -0,0 +1,119 @@
+from twisted.python import log
+from twisted.internet import reactor, ssl
+
+from autobahn.twisted.websocket import WebSocketClientFactory, \
+    WebSocketClientProtocol, \
+    connectWS
+from twisted.internet.protocol import ReconnectingClientFactory
+
+import sys, json, time
+from threading import Thread
+from os.path import dirname
+
+from mycroft.messagebus.client.ws import WebsocketClient
+from mycroft.messagebus.message import Message
+from mycroft.util.log import LOG as logger
+from mycroft.configuration import Configuration
+
+
+class MyClientProtocol(WebSocketClientProtocol):
+
+    def onConnect(self, response):
+        logger.info("Server connected: {0}".format(response.peer))
+        self.factory.emitter.emit(Message("server.connected"))
+
+    def onOpen(self):
+        logger.info("WebSocket connection open. ")
+        self.factory.emitter.emit(Message("server.websocket.open"))
+
+    def onMessage(self, payload, isBinary):
+        logger.info("status: " + self.factory.status)
+        self.factory.emitter.emit(Message("server.message.received"))
+
+    def onClose(self, wasClean, code, reason):
+        logger.info("WebSocket connection closed: {0}".format(reason))
+        self.factory.emitter.emit(Message("server.connection.closed"))
+
+    def Message_to_raw_data(self, message):
+        # convert a Message object into raw data that can be sent over
+        # websocket
+        if hasattr(message, 'serialize'):
+            return message.serialize()
+        else:
+            return json.dumps(message.__dict__)
+
+
+class MyClientFactory(WebSocketClientFactory, ReconnectingClientFactory):
+    protocol = MyClientProtocol
+
+    def __init__(self, *args, **kwargs):
+        super(MyClientFactory, self).__init__(*args, **kwargs)
+        self.client = None
+        self.status = "disconnected"
+        # mycroft_ws
+        self.emitter = None
+        self.emitter_thread = None
+        self.create_internal_emitter()
+
+    # initialize methods
+    def connect_to_internal_emitter(self):
+        self.emitter.run_forever()
+
+    def create_internal_emitter(self):
+        # connect to mycroft internal websocket
+        self.emitter = WebsocketClient()
+        self.register_internal_messages()
+        self.emitter_thread = Thread(target=self.connect_to_internal_emitter)
+        self.emitter_thread.setDaemon(True)
+        self.emitter_thread.start()
+
+    def register_internal_messages(self):
+        self.emitter.on('speak', self.handle_speak)
+        self.emitter.on('recognizer_loop:utterance', self.handle_utterance)
+
+    # websocket handlers
+    def clientConnectionFailed(self, connector, reason):
+        logger.info("Client connection failed: " + str(reason) + " .. retrying ..")
+        self.status = "disconnected"
+        self.retry(connector)
+
+    def clientConnectionLost(self, connector, reason):
+        logger.info("Client connection lost: " + str(reason) + " .. retrying ..")
+        self.status = "disconnected"
+        self.retry(connector)
+
+    # mycroft handlers
+    def handle_speak(self, event):
+        utterance = event.data.get('utterance')
+
+    def handle_utterance(self, event):
+        pass
+
+    def sendRaw(self, data):
+        if self.client is None:
+            logger.error("Client is none")
+            return
+        self.client.sendMessage(data, isBinary=True)
+
+    def sendMessage(self, type, data, context=None):
+        if self.client is None:
+            logger.error("Client is none")
+            return
+        if context is None:
+            context = {}
+        msg = self.client.Message_to_raw_data(Message(type, data, context))
+        self.client.sendMessage(msg, isBinary=False)
+        self.emitter.emit(Message("server.message.sent"))
+
+
+if __name__ == '__main__':
+
+    config = Configuration.get().get("client", {})
+    host = config.get("host", "174.59.239.227")
+    port = config.get("port", 5678)
+    adress = u"wss://" + host + u":" + str(port)
+    factory = MyClientFactory(adress)
+    factory.protocol = MyClientProtocol
+    contextFactory = ssl.ClientContextFactory()
+    reactor.connectSSL(host, port, factory, contextFactory)
+    reactor.run()
---
+++ mycroft/client/server/main.py
@@ -0,0 +1,169 @@
+from os.path import exists, dirname
+from threading import Thread
+
+from twisted.internet import reactor, ssl
+from twisted.python import log
+from twisted.internet import reactor
+from autobahn.twisted.websocket import WebSocketServerProtocol, \
+    WebSocketServerFactory
+
+from mycroft.messagebus.client.ws import WebsocketClient
+from mycroft.messagebus.message import Message
+from mycroft.util.log import LOG as logger
+from mycroft.configuration import Configuration
+from mycroft.util.ssl.self_signed import create_self_signed_cert
+
+author = "jarbas"
+
+
+# protocol
+class MyServerProtocol(WebSocketServerProtocol):
+    def onConnect(self, request):
+        logger.info("Client connecting: {0}".format(request.peer))
+
+    def onOpen(self):
+        """
+       Connection from client is opened. Fires after opening
+       websockets handshake has been completed and we can send
+       and receive messages.
+
+       Register client in factory, so that it is able to track it.
+       """
+        self.factory.register_client(self)
+        self.factory.request_client_pgp(self)
+        logger.info("WebSocket connection open.")
+
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            logger.info("Binary message received: {0} bytes".format(len(payload)))
+        else:
+            logger.info("Text message received: {0}".format(payload.decode('utf8')))
+
+        self.factory.process_message(self, payload, isBinary)
+
+    def onClose(self, wasClean, code, reason):
+        logger.info("WebSocket connection closed: {0}".format(reason))
+
+    def connectionLost(self, reason):
+        """
+       Client lost connection, either disconnected or some error.
+       Remove client from list of tracked connections.
+       """
+        self.factory.unregister_client(self, reason=u"connection lost")
+        logger.info("WebSocket connection closed: {0}".format(reason))
+
+
+# server internals
+class MyServerFactory(WebSocketServerFactory):
+    def __init__(self, *args, **kwargs):
+        super(MyServerFactory, self).__init__(*args, **kwargs)
+        # list of clients
+        self.clients = {}
+        # mycroft_ws
+        self.emitter = None
+        self.emitter_thread = None
+        self.create_internal_emitter()
+
+    def connect_to_internal_emitter(self):
+        self.emitter.run_forever()
+
+    def create_internal_emitter(self):
+        # connect to mycroft internal websocket
+        self.emitter = WebsocketClient()
+        self.register_internal_messages()
+        self.emitter_thread = Thread(target=self.connect_to_internal_emitter)
+        self.emitter_thread.setDaemon(True)
+        self.emitter_thread.start()
+
+    def register_internal_messages(self):
+        self.emitter.on('speak', self.handle_speak)
+        self.emitter.on('complete_intent_failure', self.handle_failure)
+
+    # webasocket handlers
+    def register_client(self, client):
+        """
+       Add client to list of managed connections.
+       """
+        logger.info("registering client: " + str(client.peer))
+        t, ip, sock = client.peer.split(":")
+        # see if blacklisted
+        if ip in self.ip_list and self.ip_blacklist:
+            logger.warning("Blacklisted ip tried to connect: " + ip)
+            self.unregister_client(client, reason=u"Blacklisted ip")
+            return
+        elif ip not in self.ip_list and not self.ip_blacklist:
+            logger.warning("Unknown ip tried to connect: " + ip)
+            #  if not whitelisted kick
+            self.unregister_client(client, reason=u"Unknown ip")
+            return
+        self.clients[client.peer] = {"object": client, "status": "connected"}
+
+    def unregister_client(self, client, code=3078, reason=u"unregister client request"):
+        """
+       Remove client from list of managed connections.
+       """
+        logger.info("deregistering client: " + str(client.peer))
+        if client.peer in self.clients.keys():
+            client_data = self.clients[client.peer]
+            j, ip, sock_num = client.peer.split(":")
+            context = {"user": client_data.get("names", ["unknown_user"])[0],
+                       "source": ip + ":" + str(sock_num)}
+            self.emitter.emit(
+                Message("user.disconnect",
+                        {"reason": reason, "ip": ip, "sock": sock_num},
+                        context))
+            client.sendClose(code, reason)
+            self.clients.pop(client.peer)
+
+    def process_message(self, client, payload, isBinary):
+        """
+       Process message from client, decide what to do internally here
+       """
+        logger.info("processing message from client: " + str(client.peer))
+        client_data = self.clients[client.peer]
+        client_type, ip, sock_num = client.peer.split(":")
+
+    # mycroft handlers
+    def handle_speak(self, message):
+        pass
+
+    def handle_failure(self, message):
+        pass
+
+if __name__ == '__main__':
+
+    # server
+    config = Configuration.get().get("server", {})
+    host = config.get("host", "127.0.0.1")
+    port = config.get("port", 5678)
+    max_connections = config.get("max_connections", -1)
+    adress = u"wss://" + unicode(host) + u":" + unicode(port)
+    cert = config.get("cert_file",
+                      dirname(__file__) + '/certs/JarbasServer.crt')
+    key = config.get("key_file",
+                     dirname(__file__) + '/certs/JarbasServer.key')
+
+    factory = MyServerFactory(adress)
+    factory.protocol = MyServerProtocol
+    if max_connections >= 0:
+        factory.setProtocolOptions(maxConnections=max_connections)
+
+    if not exists(key) or not exists(cert):
+        logger.warning("ssl keys dont exist, creating self signed")
+        dir = dirname(__file__) + "/certs"
+        name = key.split("/")[-1].replace(".key", "")
+        create_self_signed_cert(dir, name)
+        cert = dir + "/" + name + ".crt"
+        key = dir + "/" + name + ".key"
+        logger.info("key created at: " + key)
+        logger.info("crt created at: " + cert)
+        # update config with new keys
+        config["cert_file"] = cert
+        config["key_file"] = key
+        factory.config_update({"jarbas_server": config}, True)
+
+    # SSL server context: load server key and certificate
+    contextFactory = ssl.DefaultOpenSSLContextFactory(key, cert)
+
+    reactor.listenSSL(port, factory, contextFactory)
+    reactor.run()
