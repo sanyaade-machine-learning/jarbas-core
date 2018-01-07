a4624e12e8d09d5042f2e5e8faa3a2239ba3e6ce
jarbasai <jarbasai@mailfence.com>
2018-01-07 22:14:05 +0000

    up

---
+++ clone_server_skills.sh
@@ -0,0 +1,17 @@
+mkdir ~/server_skills
+cd ~/server_skills
+git clone https://github.com/JarbasAl/universal-fallback-aiml
+git clone https://github.com/JarbasAl/skill-joke-universal
+git clone https://github.com/JarbasAl/skill-wiki-universal
+git clone https://github.com/JarbasAl/universal-fallback-duckduckgo
+git clone https://github.com/JarbasAl/universal-fallback-wolfram-alpha
+git clone https://github.com/the7erm/mycroft-skill-diagnostics/
+git clone https://github.com/wligtenberg/coin-flip-skill/
+git clone https://github.com/btotharye/mycroft-skill-basichelp
+git clone https://github.com/MycroftAI/skill-spelling
+git clone https://github.com/MycroftAI/skill-stock
+git clone https://github.com/MycroftAI/fallback-unknown
+git clone https://github.com/MycroftAI/skill-weather
+git clone https://github.com/MycroftAI/skill-speak
+git clone https://github.com/MycroftAI/skill-personal
+git clone https://github.com/mycroftai/skill-hello-world
