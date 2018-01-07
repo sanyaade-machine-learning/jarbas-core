8eb5779d4ccb3a46427a97078818b4f8c863bc44
jarbasai <jarbasai@mailfence.com>
2018-01-07 23:57:47 +0000

    fix

--- mycroft/skills/intent_service.py
+++ mycroft/skills/intent_service.py
@@ -190,13 +190,13 @@ class IntentService(object):
         """
         return self.skill_names.get(int(skill_id), skill_id)
 
-    def handle_skill_shutdown(self, message):
+    def handle_skill_load(self, message):
         name = message.data.get("name")
-        id = message.data.get("id")
+        id = str(message.data.get("id"))
         self.skills_map[id] = name
 
-    def handle_skill_load(self, message):
-        id = message.data.get("id")
+    def handle_skill_shutdown(self, message):
+        id = str(message.data.get("id"))
         self.skills_map.pop(id)
 
     def handle_skill_manifest(self, message):
