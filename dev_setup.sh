ae21110a818b5c3ac0ccb345c8d901378085415f
jarbasai <jarbasai@mailfence.com>
2018-01-09 02:55:40 +0000

    start debug

--- dev_setup.sh
+++ dev_setup.sh
@@ -94,7 +94,6 @@ function get_config_value() {
   value="null"
   for file in ~/.mycroft/mycroft.conf /etc/mycroft/mycroft.conf $SYSTEM_CONFIG;   do
     if [[ -r $file ]] ; then
-        echo "$file"
         # remove comments
         # assume they may be preceded by whitespace, but nothing else
         parsed="$( sed 's:^\s*//.*$::g' $file )"
@@ -102,13 +101,11 @@ function get_config_value() {
         value=$( jq -r "$key" "$DIR/mycroft/configuration/sys.conf" )
         if [[ "${value}" != "null" ]] ;  then
             rm -rf $DIR/mycroft/configuration/sys.conf
-            echo "$value"
             value="null"
             return
         fi
     fi
   done
-  value="null"
   echo "$default"
 }
 
