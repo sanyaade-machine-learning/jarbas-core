98f1cf2d97633b6c4d44d5489b092518f6b4f988
jarbasai <jarbasai@mailfence.com>
2018-01-21 12:50:42 +0000

    sql db path in config

--- mycroft/server/database/user.py
+++ mycroft/server/database/user.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, Text, String, Integer, create_engine, Boolean
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 from mycroft.server.database import Base
+from mycroft.configuration.config import Configuration
+from os.path import dirname, join
 
 
 class User(Base):
@@ -16,7 +18,9 @@ class User(Base):
 
 
 class UserDatabase(object):
-    def __init__(self, path='sqlite:///users.db', debug=False):
+    def __init__(self, path=None, debug=False):
+        if path is None:
+            path = Configuration.get().get("server", {}).get("sql_user_db", join("sqlite:///", dirname(__file__), "users.db"))
         self.db = create_engine(path)
         self.db.echo = debug
 
