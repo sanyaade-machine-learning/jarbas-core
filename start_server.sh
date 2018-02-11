05972e05cba24b40f05b7ea22e17c9a0c232183d
jarbasai <jarbasai@mailfence.com>
2018-02-11 20:48:05 +0000

    hackcht

--- start_server.sh
+++ start_server.sh
@@ -2,5 +2,5 @@ screen -dmS BUS  ~/jarbas-core/start-mycroft.sh bus
 screen -dmS MICRO ~/jarbas-core/start-mycroft.sh micro
 screen -dmS SERVER ~/jarbas-core/start-mycroft.sh server
 screen -dmS SKILLS ~/jarbas-core/start-mycroft.sh skills
-screen -dmS HACKCHAT ~/jarbas-core/start-mycroft.sh hackchat
+screen -dmS HACKCHAT ~/jarbas-core/start-mycroft.sh hack
 screen -ls
