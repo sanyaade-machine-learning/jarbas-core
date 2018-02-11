072b0e7fb600e46e1418b0c394c4fdab4e85224f
jarbasai <jarbasai@mailfence.com>
2018-02-11 16:33:12 +0000

    complete_failure_context

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -1250,7 +1250,7 @@ class FallbackSkill(MycroftSkill):
             else:
                 success = priority_handler(message)
             if not success:
-                ws.emit(Message('complete_intent_failure'))
+                ws.emit(message.reply('complete_intent_failure'))
                 LOG.warning('No fallback could handle intent.')
                 #  indicate completion with exception
                 ws.emit(Message('mycroft.skill.handler.complete',
