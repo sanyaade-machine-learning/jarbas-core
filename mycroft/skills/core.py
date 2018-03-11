b0a10e6e95ab5114976fd092967010b6b9372b09
jarbasai <jarbasai@mailfence.com>
2018-03-11 00:45:43 +0000

    use events

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -263,8 +263,8 @@ class MycroftSkill(object):
             self.emitter = emitter
             self.enclosure = EnclosureAPI(emitter, self.name)
             self.__register_stop()
-            self.emitter.on('enable_intent', self.handle_enable_intent)
-            self.emitter.on('disable_intent', self.handle_disable_intent)
+            self.add_event('enable_intent', self.handle_enable_intent)
+            self.add_event('disable_intent', self.handle_disable_intent)
 
     def __register_stop(self):
         self.stop_time = time.time()
@@ -927,8 +927,6 @@ class MycroftSkill(object):
         self.settings.store()
         self.settings.is_alive = False
         # removing events
-        self.emitter.remove('enable_intent', self.handle_enable_intent)
-        self.emitter.remove('disable_intent', self.handle_disable_intent)
         for e, f in self.events:
             self.emitter.remove(e, f)
         self.events = []  # Remove reference to wrappers
